70abbee31f03a56adb7e592aa97e462eb949ccd9
jarubios <jarubios@unal.edu.co>
2020-09-28 01:59:41 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,30 @@
 ## Assingment 2 consists of setting a function that calculates the inverse of the matrix and caches it.
 ## For it to work it must be a square matrix
 
+## The arguments of the function are defined, x which is a square matrix and m which is initially set to null
+
 makeCacheMatrix <- function(x = matrix()) {
     m<-NULL
-  set<-function(y){
+ 
+## Assign the input argument to X in the parent environment and null to the m object to clear any a priori values
+    
+     set<-function(y){
     x<<-y
     m<<-NULL
   }
-  get<-function()x
+ 
+## The input argument of the value of m is assigned in the parent environment
+     
+      get<-function()x
   setinverse<-function(inverse) m<<-inverse
   getinverse<-function() m
+  
+## The list of items are named
+  
   list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
+## Function cachesolve
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
